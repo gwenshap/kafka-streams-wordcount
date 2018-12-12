1454db536dda5ab01240d91b692a08fd842351f4
Daniel Pacak <pacak.daniel@gmail.com>
2018-12-12 20:59:33 +0100

    refactor: Remove useless comments in the code and get rid of build warnings

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-This example counts words from 'wordcount-input' topic, excluding the word "the", and writes the counts to 'wordcount-output' topic. It is based on Confluent's wordcount example, with very minor changes. You can find the original here:
-https://github.com/confluentinc/examples/blob/3.1.x/kafka-streams/src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
+# kafka-streams-wordcount
+
+This example counts words from `wordcount-input` topic, excluding the word "the", and writes the counts to
+'wordcount-output' topic.
 
 To run this example:
 
 0. Build the project with `mvn package`, this will generate an uber-jar with the streams app and all its dependencies.
-1. Create a wordcount-input topic:
+1. Create a `wordcount-input` topic:
 
     `bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic wordcount-input --partitions 1 --replication-factor 1`
 
-2. Produce some text to the topic. Don't forget to repeat words (so we can count higher than 1) and to use the word "the", so we can filter it.
+2. Produce some text to the topic. Don't forget to repeat words (so we can count higher than 1) and to use the word "the",
+   so we can filter it.
 
    `bin/kafka-console-producer.sh --broker-list localhost:9092 --topic wordcount-input`
 
@@ -29,8 +32,3 @@ If you want to reset state and re-run the application (maybe with some changes?)
 2. (optional) Delete the output topic:
 
     `bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic wordcount-output`
-
-
-
-
-
--- src/main/java/com/shapira/examples/streams/wordcount/WordCountExample.java
+++ src/main/java/com/shapira/examples/streams/wordcount/WordCountExample.java
@@ -1,7 +1,6 @@
 package com.shapira.examples.streams.wordcount;
 
 
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -16,49 +15,35 @@ import java.util.regex.Pattern;
 
 public class WordCountExample {
 
-    public static void main(String[] args) throws Exception{
+    private static final String INPUT_TOPIC = "wordcount-input";
 
+    private static final String OUTPUT_TOPIC = "wordcount-output";
+
+    private static final Pattern SPLIT_WORDS_PATTERN = Pattern.compile("\\W+");
+
+    public static void main(String[] args) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-
-        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
-        // Note: To re-run the demo, you need to use the offset reset tool:
-        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        // work-around for an issue around timing of creating internal topics
-        // Fixed in Kafka 0.10.2.0
-        // don't use in large production apps - this increases network load
-        // props.put(CommonClientConfigs.METADATA_MAX_AGE_CONFIG, 500);
 
         KStreamBuilder builder = new KStreamBuilder();
 
-        KStream<String, String> source = builder.stream("wordcount-input");
+        KStream<String, String> source = builder.stream(INPUT_TOPIC);
 
-
-        final Pattern pattern = Pattern.compile("\\W+");
-        KStream counts  = source.flatMapValues(value-> Arrays.asList(pattern.split(value.toLowerCase())))
-                .map((key, value) -> new KeyValue<Object, Object>(value, value))
+        KStream counts = source.flatMapValues(value -> Arrays.asList(SPLIT_WORDS_PATTERN.split(value.toLowerCase())))
+                .map((key, value) -> new KeyValue<>(value, value))
                 .filter((key, value) -> (!value.equals("the")))
                 .groupByKey()
-                .count("CountStore").mapValues(value->Long.toString(value)).toStream();
-        counts.to("wordcount-output");
+                .count("WordCountStore")
+                .mapValues(value -> Long.toString(value)).toStream();
+        counts.to(OUTPUT_TOPIC);
 
         KafkaStreams streams = new KafkaStreams(builder, props);
 
-        // This is for reset to work. Don't use in production - it causes the app to re-load the state from Kafka on every start
-        streams.cleanUp();
-
         streams.start();
-
-        // usually the stream application would be running forever,
-        // in this example we just let it run for some time and stop since the input data is finite.
-        Thread.sleep(5000L);
-
-        streams.close();
-
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
